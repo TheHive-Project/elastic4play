52b2a44462a8de00cfc2f00b46b589998be6587d
To-om <toom@thehive-project.org>
2017-09-08 15:37:16 +0200

    Fix error 500 when user roles are insufficient

--- app/org/elastic4play/controllers/Authenticated.scala
+++ app/org/elastic4play/controllers/Authenticated.scala
@@ -11,7 +11,7 @@ import play.api.{ Configuration, Logger }
 import play.api.http.HeaderNames
 import play.api.mvc._
 
-import org.elastic4play.AuthenticationError
+import org.elastic4play.{ AuthenticationError, AuthorizationError }
 import org.elastic4play.services.{ AuthContext, AuthSrv, Role, UserSrv }
 import org.elastic4play.utils.Instance
 
@@ -173,7 +173,7 @@ class Authenticated(
           block(new AuthenticatedRequest(authContext, request))
             .map(result ⇒ setSessingUser(result, authContext)(request))
         else
-          Future.failed(new Exception(s"Insufficient rights to perform this action"))
+          Future.failed(AuthorizationError(s"Insufficient rights to perform this action"))
       }
     }
   }
