a056e7dce0dbc651b6540556ed5c231c9ac4e057
To-om <toom@thehive-project.org>
2018-02-19 16:37:40 +0100

    Add method to remove entity by providing this entity (not only its id)

--- app/org/elastic4play/services/DeleteSrv.scala
+++ app/org/elastic4play/services/DeleteSrv.scala
@@ -3,12 +3,12 @@ package org.elastic4play.services
 import javax.inject.{ Inject, Singleton }
 
 import scala.concurrent.{ ExecutionContext, Future }
-
 import play.api.libs.json.JsObject
-
 import org.elastic4play.NotFoundError
 import org.elastic4play.database.{ DBRemove, ModifyConfig }
-import org.elastic4play.models.{ AbstractModelDef, EntityDef }
+import org.elastic4play.models.{ AbstractModelDef, BaseEntity, EntityDef }
+
+import scala.util.Success
 
 @Singleton
 class DeleteSrv @Inject() (
@@ -19,11 +19,14 @@ class DeleteSrv @Inject() (
     implicit val ec: ExecutionContext) {
 
   def apply[M <: AbstractModelDef[M, E], E <: EntityDef[M, E]](model: M, id: String)(implicit authContext: AuthContext): Future[E] = {
-    for {
-      entity ← getSrv[M, E](model, id)
-      newEntity ← updateSrv.doUpdate(entity, model.removeAttribute, ModifyConfig.default)
-      _ = eventSrv.publish(AuditOperation(newEntity, AuditableAction.Delete, JsObject.empty, authContext))
-    } yield newEntity
+    getSrv[M, E](model, id).flatMap(entity ⇒ apply(entity))
+  }
+
+  def apply[E <: BaseEntity](entity: E)(implicit authContext: AuthContext) = {
+    updateSrv.doUpdate(entity, entity.model.removeAttribute, ModifyConfig.default)
+      .andThen {
+        case Success(newEntity) ⇒ eventSrv.publish(AuditOperation(newEntity, AuditableAction.Delete, JsObject.empty, authContext))
+      }
   }
 
   def realDelete[M <: AbstractModelDef[M, E], E <: EntityDef[M, E]](model: M, id: String)(implicit authContext: AuthContext): Future[Unit] = {
