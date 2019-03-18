e1a88d90e48fab4ef65c46d13f59987576e9d5a8
To-om <toom@thehive-project.org>
2019-03-18 15:40:36 +0100

    #81 reverts default search operator (wildcard => term)

--- app/org/elastic4play/services/JsonFormat.scala
+++ app/org/elastic4play/services/JsonFormat.scala
@@ -39,7 +39,7 @@ object JsonFormat {
 
   implicit val roleWrites: Writes[Role] = Writes[Role](role ⇒ JsString(role.name))
 
-  implicit def configWrites: OWrites[Configuration] = OWrites { (cfg: Configuration) ⇒
+  implicit def configWrites: OWrites[Configuration] = OWrites[Configuration] { cfg ⇒
     JsObject(cfg.subKeys.map(key ⇒ key → configValueWrites.writes(cfg.underlying.getValue(key))).toSeq)
   }
 
@@ -139,63 +139,59 @@ object JsonFormat {
   }
 
   implicit val queryReads: Reads[QueryDef] = {
-    Reads { (json: JsValue) ⇒
-      json match {
-        case JsObjOne(("_and", JsArray(v)))               ⇒ JsSuccess(and(v.map(_.as[QueryDef]): _*))
-        case JsObjOne(("_or", JsArray(v)))                ⇒ JsSuccess(or(v.map(_.as[QueryDef]): _*))
-        case JsObjOne(("_contains", JsString(v)))         ⇒ JsSuccess(contains(v))
-        case JsObjOne(("_not", v: JsObject))              ⇒ JsSuccess(not(v.as[QueryDef]))
-        case JsObjOne(("_any", _))                        ⇒ JsSuccess(any)
-        case j: JsObject if j.fields.isEmpty              ⇒ JsSuccess(any)
-        case JsObjOne(("_gt", JsObjOne(n, JsVal(v))))     ⇒ JsSuccess(n ~> v)
-        case JsObjOne(("_gte", JsObjOne(n, JsVal(v))))    ⇒ JsSuccess(n ~>= v)
-        case JsObjOne(("_lt", JsObjOne(n, JsVal(v))))     ⇒ JsSuccess(n ~< v)
-        case JsObjOne(("_lte", JsObjOne(n, JsVal(v))))    ⇒ JsSuccess(n ~<= v)
-        case JsObjOne(("_between", JsRange(n, f, t)))     ⇒ JsSuccess(n ~<> (f → t))
-        case JsObjOne(("_parent", JsParent(p, q)))        ⇒ JsSuccess(parent(p, q))
-        case JsObjOne(("_parent", JsParentId(p, i)))      ⇒ JsSuccess(withParent(p, i))
-        case JsObjOne(("_id", JsString(id)))              ⇒ JsSuccess(withId(id))
-        case JsField(field, value)                        ⇒ JsSuccess(field ~= value)
-        case JsObjOne(("_child", JsParent(p, q)))         ⇒ JsSuccess(child(p, q))
-        case JsObjOne(("_string", JsString(s)))           ⇒ JsSuccess(string(s))
-        case JsObjOne(("_in", JsFieldIn(f, v)))           ⇒ JsSuccess(f in (v: _*))
-        case JsObjOne(("_type", JsString(v)))             ⇒ JsSuccess(ofType(v))
-        case JsObjOne(("_like", JsField(field, value)))   ⇒ JsSuccess(field like value)
-        case JsObjOne(("_strict", JsField(field, value))) ⇒ JsSuccess(field ~== value)
-        case JsObjOne((n, JsVal(v))) ⇒
-          if (n.startsWith("_")) logger.warn(s"""Potentially invalid search query : {"$n": "$v"}"""); JsSuccess(n ~= v)
-        case other ⇒ JsError(s"Invalid query: unexpected $other")
-      }
+    Reads {
+      case JsObjOne(("_and", JsArray(v)))                 ⇒ JsSuccess(and(v.map(_.as[QueryDef]): _*))
+      case JsObjOne(("_or", JsArray(v)))                  ⇒ JsSuccess(or(v.map(_.as[QueryDef]): _*))
+      case JsObjOne(("_contains", JsString(v)))           ⇒ JsSuccess(contains(v))
+      case JsObjOne(("_not", v: JsObject))                ⇒ JsSuccess(not(v.as[QueryDef]))
+      case JsObjOne(("_any", _))                          ⇒ JsSuccess(any)
+      case j: JsObject if j.fields.isEmpty                ⇒ JsSuccess(any)
+      case JsObjOne(("_gt", JsObjOne(n, JsVal(v))))       ⇒ JsSuccess(n ~> v)
+      case JsObjOne(("_gte", JsObjOne(n, JsVal(v))))      ⇒ JsSuccess(n ~>= v)
+      case JsObjOne(("_lt", JsObjOne(n, JsVal(v))))       ⇒ JsSuccess(n ~< v)
+      case JsObjOne(("_lte", JsObjOne(n, JsVal(v))))      ⇒ JsSuccess(n ~<= v)
+      case JsObjOne(("_between", JsRange(n, f, t)))       ⇒ JsSuccess(n ~<> (f → t))
+      case JsObjOne(("_parent", JsParent(p, q)))          ⇒ JsSuccess(parent(p, q))
+      case JsObjOne(("_parent", JsParentId(p, i)))        ⇒ JsSuccess(withParent(p, i))
+      case JsObjOne(("_id", JsString(id)))                ⇒ JsSuccess(withId(id))
+      case JsField(field, value)                          ⇒ JsSuccess(field ~= value)
+      case JsObjOne(("_child", JsParent(p, q)))           ⇒ JsSuccess(child(p, q))
+      case JsObjOne(("_string", JsString(s)))             ⇒ JsSuccess(string(s))
+      case JsObjOne(("_in", JsFieldIn(f, v)))             ⇒ JsSuccess(f in (v: _*))
+      case JsObjOne(("_type", JsString(v)))               ⇒ JsSuccess(ofType(v))
+      case JsObjOne(("_like", JsField(field, value)))     ⇒ JsSuccess(field like value)
+      case JsObjOne(("_wildcard", JsField(field, value))) ⇒ JsSuccess(field ~=~ value)
+      case JsObjOne((n, JsVal(v))) ⇒
+        if (n.startsWith("_")) logger.warn(s"""Potentially invalid search query : {"$n": "$v"}"""); JsSuccess(n ~= v)
+      case other ⇒ JsError(s"Invalid query: unexpected $other")
     }
   }
 
-  implicit val aggReads: Reads[Agg] = Reads { (json: JsValue) ⇒
-    json match {
-      case JsAgg("avg", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectAvg(aggregationName, field, query))
-      case JsAgg("min", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectMin(aggregationName, field, query))
-      case JsAgg("max", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectMax(aggregationName, field, query))
-      case JsAgg("sum", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectSum(aggregationName, field, query))
-      case JsAgg("count", aggregationName, _)                           ⇒ JsSuccess(selectCount(aggregationName, (json \ "_query").asOpt[QueryDef]))
-      case JsAgg("top", aggregationName, _) ⇒
-        val size = (json \ "_size").asOpt[Int].getOrElse(10)
-        val order = (json \ "_order").asOpt[Seq[String]].getOrElse(Nil)
-        JsSuccess(selectTop(aggregationName, size, order))
-      case JsAgg("time", aggregationName, _) ⇒
-        val fields = (json \ "_fields").as[Seq[String]]
-        val interval = (json \ "_interval").as[String]
-        val selectables = (json \ "_select").as[Seq[Agg]]
-        JsSuccess(groupByTime(aggregationName, fields, interval, selectables: _*))
-      case JsAgg("field", aggregationName, _) ⇒
-        val field = (json \ "_field").as[String]
-        val size = (json \ "_size").asOpt[Int].getOrElse(10)
-        val order = (json \ "_order").asOpt[Seq[String]].getOrElse(Nil)
-        val selectables = (json \ "_select").as[Seq[Agg]]
-        JsSuccess(groupByField(aggregationName, field, size, order, selectables: _*))
-      case JsAgg("category", aggregationName, _) ⇒
-        val categories = (json \ "_categories").as[Map[String, QueryDef]]
-        val selectables = (json \ "_select").as[Seq[Agg]]
-        JsSuccess(groupByCaterogy(aggregationName, categories, selectables: _*))
-    }
+  implicit val aggReads: Reads[Agg] = Reads {
+    case JsAgg("avg", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectAvg(aggregationName, field, query))
+    case JsAgg("min", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectMin(aggregationName, field, query))
+    case JsAgg("max", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectMax(aggregationName, field, query))
+    case JsAgg("sum", aggregationName, JsAggFieldQuery(field, query)) ⇒ JsSuccess(selectSum(aggregationName, field, query))
+    case json @ JsAgg("count", aggregationName, _)                    ⇒ JsSuccess(selectCount(aggregationName, (json \ "_query").asOpt[QueryDef]))
+    case json @ JsAgg("top", aggregationName, _) ⇒
+      val size = (json \ "_size").asOpt[Int].getOrElse(10)
+      val order = (json \ "_order").asOpt[Seq[String]].getOrElse(Nil)
+      JsSuccess(selectTop(aggregationName, size, order))
+    case json @ JsAgg("time", aggregationName, _) ⇒
+      val fields = (json \ "_fields").as[Seq[String]]
+      val interval = (json \ "_interval").as[String]
+      val selectables = (json \ "_select").as[Seq[Agg]]
+      JsSuccess(groupByTime(aggregationName, fields, interval, selectables: _*))
+    case json @ JsAgg("field", aggregationName, _) ⇒
+      val field = (json \ "_field").as[String]
+      val size = (json \ "_size").asOpt[Int].getOrElse(10)
+      val order = (json \ "_order").asOpt[Seq[String]].getOrElse(Nil)
+      val selectables = (json \ "_select").as[Seq[Agg]]
+      JsSuccess(groupByField(aggregationName, field, size, order, selectables: _*))
+    case json @ JsAgg("category", aggregationName, _) ⇒
+      val categories = (json \ "_categories").as[Map[String, QueryDef]]
+      val selectables = (json \ "_select").as[Seq[Agg]]
+      JsSuccess(groupByCaterogy(aggregationName, categories, selectables: _*))
   }
 
   implicit val authContextWrites: OWrites[AuthContext] = OWrites[AuthContext] { authContext ⇒
